342428f98259423a209f01244f1073adee712a9d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-02-07 09:05:14 -0800

    git

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-"""Launches a Flask web application.
-The app runs on 0.0.0.0 at port 5000.
-Routes:
-    /hbnb: Displays the HBnB home page.
 """
+A script that starts a Flask web application
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
 from models import storage
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    places = storage.all("Place")
-    return render_template("100-hbnb.html",
-                           states=states, amenities=amenities, places=places)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """Function to remove the current SQLAlchemy Session"""
     storage.close()
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function that displays HBNB web page"""
+    st = storage.all(State).values()
+    st = sorted(st, key=lambda k: k.name)
+    sc = []
+
+    for state in st:
+        sc.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    am = storage.all(Amenity).values()
+    am = sorted(am, key=lambda k: k.name)
+
+    pl = storage.all(Place).values()
+    pl = sorted(pl, key=lambda k: k.name)
+
+    return render_template('100-hbnb.html', states=sc, amenities=am, places=pl)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
